29efea5de43bf5397a1c77e8f53e242402a901be
Yardley <D.Yardley3767@student.leedsbeckett.ac.uk>
2024-02-01 15:42:02 +0000

    Testing adding src folder?

---
+++ WeeklyExercises/1GettingStarted/src/Test.java
@@ -0,0 +1,11 @@
+public class Test
+    // Test" needs to be the same spelling and format as the name of the program.
+{
+    //----------------------------------------------------
+    //  Prints a statement.
+    //----------------------------------------------------
+    public static void main (String[] args)
+    {
+        System.out.println ("An Emergency Broadcast");
+    }
+}
