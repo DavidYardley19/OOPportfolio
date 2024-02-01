14a38a4fc50e37629afe1e59534bb57d07e49e52
Yardley <D.Yardley3767@student.leedsbeckett.ac.uk>
2024-02-01 16:03:50 +0000

    adding files

---
+++ WeeklyExercises/1GettingStarted/src/PersonalDetails.java
@@ -0,0 +1,8 @@
+public class PersonalDetails {
+
+    public static void main(String[] args)
+    {
+        System.out.println("David\nMy address\nMy postcode");
+    }
+
+}
--- WeeklyExercises/1GettingStarted/src/Test.java
+++ WeeklyExercises/1GettingStarted/src/Test.java
@@ -1,11 +1,14 @@
 public class Test
-    // Test" needs to be the same spelling and format as the name of the program.
+    // 1. Test" needs to be the same spelling and format as the name of the program.
 {
-    //----------------------------------------------------
-    //  Prints a statement.
-    //----------------------------------------------------
     public static void main (String[] args)
+            //4. Chaniging main to man will cause some issues since you need to have at least one "main" for the program to run
     {
         System.out.println ("An Emergency Broadcast");
+        //2. Changing the string will not affect the program
+        //3. yoyu can not remove the first quote
+        //5. There is no such method as "bogus"... we can not change println
+        //6. we can not remove the semi colon at the end of println
     }
+    //7. There will need to be a curly braces
 }
