21e2feb1aa3b6075e527712a1b0341f0e3a6a397
Yardley <D.Yardley3767@student.leedsbeckett.ac.uk>
2024-02-01 16:14:34 +0000

    final changes

---
+++ WeeklyExercises/1GettingStarted/src/Questions.java
@@ -0,0 +1,15 @@
+public class Questions {
+
+    public static void main(String[] args){
+        System.out.println("What is the latest version of Java SDK?\n\t JDK 21");
+
+        System.out.println("What is the difference beteen Java SE and Java ME?");
+
+        System.out.println("What OS is java available for?\n\t Windows, it was mentioned that there would be some issues with MAC");
+
+        System.out.println("What is the most popular IDE for Java?\n\t ");
+
+        System.out.println("What is the main() function for in a java program?\n\t It is the first method called after compiling a program");
+    }
+
+}
