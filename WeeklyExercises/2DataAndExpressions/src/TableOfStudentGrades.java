0887eba48013f6636e312144d9089551145f01a3
DavidYardley19 <d.yardley3767@student.leedsbeckett.ac.uk>
2024-02-04 01:38:55 +0000

    first task complete

---
+++ WeeklyExercises/2DataAndExpressions/src/TableOfStudentGrades.java
@@ -0,0 +1,34 @@
+public class TableOfStudentGrades
+{
+    public static void main(String[] args)
+    {
+
+        System.out.println(
+                "The requirements for the program are as follows:\n" +
+                "\n" +
+                "Print the border on the top as illustrated (using the slash and backslash characters).\n" +
+                        // Basic Code met
+                "Use tab characters to get your columns aligned and you must use the + operator both for addition and string concatenation.\n" +
+                        // Basic Code met
+                "Make up your own student names and points—the ones shown are just for illustration purposes. You need 5 names.");
+                        // Basic Code met
+
+        System.out.println();
+
+        // Basic Code
+        System.out.println("///////////////////"+"\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        System.out.println("==\t\t\tStudent Points\t\t\t==");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" + "///////////////////");
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Name\t\t\tLab\t\tBonus\tTotal");
+        System.out.println("----\t\t\t---\t\t-----\t-----");
+        System.out.println("Adam\t\t\t43\t\t7\t\t50");
+        System.out.println("Beth\t\t\t10\t\t7\t\t17");
+        System.out.println("Cole\t\t\t12\t\t7\t\t12");
+        System.out.println("Dave\t\t\t64\t\t7\t\t71");
+        System.out.println("Emil\t\t\t12\t\t7\t\t19");
+
+    }
+}
