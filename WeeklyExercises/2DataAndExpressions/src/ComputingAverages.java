4b9da8fcc7e400b5b60c26d7e3f5494eab341200
DavidYardley19 <d.yardley3767@student.leedsbeckett.ac.uk>
2024-02-08 11:49:45 +0000

    Finished wk2 ex2: average of 3 int

---
+++ WeeklyExercises/2DataAndExpressions/src/ComputingAverages.java
@@ -0,0 +1,37 @@
+// *******************************************************************
+//   Average.java
+//
+//   Read three integers from the user and print their average
+// *******************************************************************
+
+import java.util.Scanner;
+import java.util.function.ToIntFunction;
+
+public class ComputingAverages
+{
+        public static void main(String[] args)
+        {
+            int val1, val2, val3;
+            double average;
+            Scanner scan = new Scanner(System.in) ;
+
+            // get three values from user
+            System.out.println("Please enter three integers and " +
+                    "I will compute their average");
+            
+            System.out.println("Enter the first integer: ");
+            val1 = scan.nextInt();
+
+            System.out.println("Enter the second integer: ");
+            val2 = scan.nextInt();
+
+            System.out.println("Enter the third integer: ");
+            val3 = scan.nextInt();
+
+            average = (val1 + val2 + val3) / 3;
+
+            System.out.println("The average is: " + average);
+
+        }
+}
+
