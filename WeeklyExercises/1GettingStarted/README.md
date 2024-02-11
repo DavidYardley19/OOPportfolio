3b0b523532779e727b037bc61180422103b1467a
DavidYardley19 <d.yardley3767@student.leedsbeckett.ac.uk>
2024-02-11 15:09:27 +0000

    Some questions amended. Note: Still a bigger fan of visual studio however this may be due to comfortability with C# experience.

---
+++ WeeklyExercises/1GettingStarted/README.md
@@ -0,0 +1,27 @@
+# Week 1 > Getting Started
+## Find the below as a list of program outputs.\
+
+### Hello World
+Hello world
+
+### Test
+An Emergency Broadcast
+
+NOTE: It is best to look at the code, to see what comments have been made.
+
+### Personal Details
+David
+My address
+My postcode
+
+### Diamonds
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+
+### Questions
+
--- WeeklyExercises/1GettingStarted/src/Questions.java
+++ WeeklyExercises/1GettingStarted/src/Questions.java
@@ -1,15 +1,18 @@
 public class Questions {
 
     public static void main(String[] args){
-        System.out.println("What is the latest version of Java SDK?\n\t JDK 21");
+        System.out.println("What is the latest version of Java SDK?\n\t JDK 21\n");
 
-        System.out.println("What is the difference beteen Java SE and Java ME?");
+        System.out.println("What is the difference beteen Java SE and Java ME?\n" +
+                "\t SE Stands for standard-edition, providing core functionality\n" +
+                "\t ME stands for micro-edition, providing API's for apps targetting mobiles and embedded devices.\n");
 
-        System.out.println("What OS is java available for?\n\t Windows, it was mentioned that there would be some issues with MAC");
+        System.out.println("What OS is java available for?\n\t Windows, it was mentioned that there would be some issues with MAC\n");
 
-        System.out.println("What is the most popular IDE for Java?\n\t ");
+        System.out.println("What is the most popular IDE for Java?\n\t IntelliJ\n" +
+                "\t However, VS Code has become fully featured making it hard to distinguish between a code editor and an IDE\n");
 
-        System.out.println("What is the main() function for in a java program?\n\t It is the first method called after compiling a program");
+        System.out.println("What is the main() function for in a java program?\n\t It is the first method called after compiling a program\n");
     }
 
 }
